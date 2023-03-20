95804c769a1e3e641104d282bea0ba8fe00db44a
David Proto <davidproto@sbcglobal.net>
2023-03-19 23:31:20 -0400

    search functionality now working

--- src/App.js
+++ src/App.js
@@ -8,15 +8,18 @@ import useAxios from "./hooks/useAxios";
 export const ImageContext = createContext();
 
 function App()  {
+  const [searchImage, setSearchImage] = useState("");
   const { response, isLoading, error, fetchData } = useAxios(
-    `/search/photos?page=1&query=office&client_id=${process.env
+    `/search/photos?page=1&query=sunsets&client_id=${process.env
       .REACT_APP_ACCESS_KEY}`);
   
   const value = {
     response,
     isLoading,
     error,
-    fetchData
+    fetchData,
+    searchImage,
+    setSearchImage
   }
 
   return (
--- src/components/searchField.js
+++ src/components/searchField.js
@@ -1,19 +1,53 @@
+import { useState, useContext } from "react";
+import { ImageContext } from "../App";
+
 const SearchField = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const { fetchData, setSearchImage } = useContext(ImageContext);
+
+  const handleInputChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const handleButtonSearch = () => {
+    fetchData(`/search/photos?page=1&per_page=30&query=${searchValue}
+    &client_id=${process.env.REACT_APP_ACCESS_KEY}`);
+    setSearchValue("");
+    setSearchImage(searchValue);
+  };
+
+  const handleEnterSearch = (e) => {
+    if (e.key === "Enter") {
+      fetchData(`/search/photos?page=1&per_page=30&query=${searchValue}
+      &client_id=${process.env.REACT_APP_ACCESS_KEY}`);
+      setSearchValue("");
+      setSearchImage(searchValue);
+    }
+  };
+
   return (
-    <div className="flex"><br></br>   
-       📸
+    <div className="flex">
+      <br></br>
+      📸
       <input
         className="bg-gray-50 border-gray-800 text-sm w-full
         indent-2 p-2.5 outline-none focus:border-gray-800 focus:ring-2
         rounded-tl rounded-bl"
         type="search"
         placeholder="Search for anything...."
+        value={searchValue}
+        onChange={handleInputChange}
+        onKeyDown={handleEnterSearch}
       />
-      <button 
+      <button
+        onClick={handleButtonSearch}
+        disabled={!searchValue}
         className=" bg-pink-600 px-6 py-2.5 text-white text-sm text-center
          rounded-tr rounded-br focus:ring-2 focus:ring-gray-700 
          disabled:bg-gray-400"
-      >Search</button>
+      >
+        Search
+      </button>
     </div>
   );
 };
